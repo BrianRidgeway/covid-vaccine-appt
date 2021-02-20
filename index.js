1db0ea80f2842be545c131b2da9da6efc9d8622f
Brian Ridgeway <brianjridgeway@gmail.com>
2021-02-20 12:09:59 -0500

    hard code ua

--- index.js
+++ index.js
@@ -37,7 +37,8 @@ const vaccinate = async() => {
     if( process.env.DEBUG ){
       console.log( "Page opened" );
     }
-    let agent = ua.getRandom( (ua) => { return ua.browserName === 'Firefox' });
+    //let agent = ua.getRandom( (ua) => { return ua.browserName === 'Firefox' });
+    let agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:85.0) Gecko/20100101 Firefox/85.0';
     if( process.env.DEBUG ){
       console.log( `Setting UA ${agent}...` );
     }
