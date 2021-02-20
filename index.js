4e1b0e8cb36ce4090109352100fc556fad17c6f5
Brian Ridgeway <brianjridgeway@gmail.com>
2021-02-20 13:27:19 -0500

    adding giant

--- index.js
+++ index.js
@@ -17,7 +17,23 @@ const setConfig = () => {
   if( cfg.maxWait === undefined ){
     cfg.maxWait = 0;
   }
+  if( cfg.pageLoadWait === undefined ){
+    cfg.pageLoadWait = 5000;
+  }
   return cfg;
+}
+
+const giant = async(page) => {
+  let giantStatus = await page.$eval( '#divApptTypeInfo0', n => n.innerText);
+  if( giantStatus.match( /There are currently no COVID-19 vaccine appointments available/) ){
+    if( process.env.DEBUG ){
+      console.log( `GIANT - ${giantStatus}` );
+    }
+  }
+  else{
+    console.log( 'Giant has appointments!' );
+    console.log( giantStatus );
+  }
 }
 
 const cvs = async(page) => {
@@ -29,7 +45,7 @@ const cvs = async(page) => {
   if( process.env.DEBUG ){
     console.log( `Waiting for modal to open...` );
   }
-  const modalEH = await page.waitForSelector( `#vaccineinfo-${config.state}`, { visible: true, timeout: 5000} );
+  const modalEH = await page.waitForSelector( `#vaccineinfo-${config.state}`, { visible: true, timeout: config.pageLoadWait} );
 
   if( process.env.DEBUG ){
     console.log( `Reading the modal availability table...` );
@@ -39,10 +55,10 @@ const cvs = async(page) => {
     const cityName = await siteInfo.$eval( 'span.city', n => n.innerText);
     const cityStatus = await siteInfo.$eval( 'span.status', n => n.innerText);
     if( cityStatus != "Fully Booked" ){
-      console.log( cityName + ": " + cityStatus );
+      console.log( `CVS - ${cityName}: ${cityStatus}` );
     }
     if( process.env.DEBUG ){
-      console.log( `${cityName}: ${cityStatus}` );
+      console.log( `CVS - ${cityName}: ${cityStatus}` );
     }
 
   };
@@ -76,14 +92,25 @@ const vaccinate = async() => {
       console.log( `Waiting ${waitTime}ms...` );
     }
     await page.waitForTimeout( waitTime );
-
-    if( process.env.DEBUG ){
-      console.log( `Goto page ${config.url}...` );
+    let urls;
+    if( Array.isArray( config.url ) ){
+      urls = config.url;
     }
-    await page.goto( config.url, {waitUntil: 'domcontentloaded', timeout: 5000});
+    else{
+      urls = [ config.url ];
+    }
+    for( const url of urls ){
+      if( process.env.DEBUG ){
+        console.log( `Goto page ${url}` );
+      }
+      await page.goto( url, {waitUntil: 'domcontentloaded', timeout: config.pageLoadWait});
 
-    if( config.url.match( 'cvs' ) ){
-      await cvs(page);
+      if( url.match( /cvs/i ) ){
+        await cvs(page);
+      }
+      else if( url.match( /giant/i ) ){
+        await giant(page);
+      }
     }
   } catch(e){
     console.log(`ERROR: ${e}`);
